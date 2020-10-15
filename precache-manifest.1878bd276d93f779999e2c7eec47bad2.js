871a6086681954eaeeb49e6c2426f121666b9d03
Fighter <farmanara.code@gmail.com>
2020-10-16 02:56:57 +0330

    Updates

---
+++ precache-manifest.1878bd276d93f779999e2c7eec47bad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2073dec520f43ec329a9399670c98d62",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "a43280c00a30cedff0ce",
+    "url": "/calc/static/css/main.6c8ecc9c.chunk.css"
+  },
+  {
+    "revision": "4d0137883619c7fa9360",
+    "url": "/calc/static/js/2.e0784415.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/calc/static/js/2.e0784415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a43280c00a30cedff0ce",
+    "url": "/calc/static/js/main.6d765585.chunk.js"
+  },
+  {
+    "revision": "a4a3cc8b99add8deabba",
+    "url": "/calc/static/js/runtime-main.6bd3be6f.js"
+  }
+]);
